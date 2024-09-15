2dfc22e2d7da0325877fc09f22728989fa0b90f1
Kaustubh Khavnekar <kaustubhkhavnekar@gmail.com>
2024-09-15 19:23:45 -0400

    Update subnet CIDRs

--- networking-terraform/variables.tf
+++ networking-terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "availability_zones" {
   description = "List of availability zones to use"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "use_managed_nat" {
@@ -25,13 +25,13 @@ variable "vpc_cidr" {
 variable "vpc_private_cidrs" {
   type        = list(string)
   description = "CIDRs for VPC private subnets"
-  default     = ["10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20", "10.0.48.0/20"]
+  default     = ["10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20", "10.0.48.0/20", "10.0.64.0/20", "10.0.80.0/20", ]
 }
 
 variable "vpc_public_cidrs" {
   type        = list(string)
   description = "CIDRs for VPC public subnets"
-  default     = ["10.0.64.0/20", "10.0.80.0/20", "10.0.96.0/20", "10.0.112.0/20"]
+  default     = ["10.0.96.0/20", "10.0.112.0/20", "10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20", "10.0.176.0/20"]
 }
 
 variable "tags" {
