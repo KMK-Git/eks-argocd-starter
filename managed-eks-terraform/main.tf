c1456a4ce60d888fb0a258cb1f7ed05eae20bb2b
Kaustubh Khavnekar <kaustubhkhavnekar@gmail.com>
2024-09-18 23:33:50 -0400

    Fix depends on

--- managed-eks-terraform/main.tf
+++ managed-eks-terraform/main.tf
@@ -150,7 +150,7 @@ resource "aws_iam_role_policy_attachment" "argocd_admin_assume_role_policy_attac
 
 resource "helm_release" "argocdmanagedcluster" {
   provider         = helm.argocdcluster
-  depends_on       = [module.central_eks, aws_iam_role_policy_attachment.argocd_admin_assume_role_policy_attachment]
+  depends_on       = [module.managed_eks, aws_iam_role_policy_attachment.argocd_admin_assume_role_policy_attachment]
   name             = "argocdmanagedcluster"
   chart            = "${path.module}/../charts/argocdmanagedcluster"
   namespace        = "argocd"
